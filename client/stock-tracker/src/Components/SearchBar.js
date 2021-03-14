c5984de9eed9c13c34818a8770a655185a3601a8
Bhavin <bhavin1625p@gmail.com>
2021-03-14 15:56:51 +0530

    sugg only on button click

--- client/stock-tracker/src/Components/SearchBar.js
+++ client/stock-tracker/src/Components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 // to add search bar suggestions and make api calls on submit
@@ -7,7 +7,7 @@ function SearchBar() {
 
     const apikey = "B1304M15MQ95KRG9";
 
-    const [inputt, setinputt] = useState("");  
+    const [inputt, setinputt] = useState("");
     const [suggArray, setsuggArray] = useState([]); // to store the suggestion names 
 
     const handleChange = (e) => {
@@ -16,6 +16,7 @@ function SearchBar() {
 
     const searchStock = (e) => {
         e.preventDefault();
+        console.log("hello")
         if (inputt.length !== 0) {
             //here to create an api key to make the req and display the json data
             axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${inputt}&apikey=${apikey}`)
@@ -37,12 +38,12 @@ function SearchBar() {
         // here to make the api request and also create stock context to store current stock information  based on the inputt state
     }
 
-    
+
     return (
         <>
             <div className="">
                 <input type="text" className=" w-75 p-lg-2 m-lg-5 h-25" placeholder="Enter the stock name you want to search for: " onChange={handleChange} />
-                <button onclick={searchStock} className="btn btn-primary btn-lg"> Search </button>
+                <button onClick={searchStock} className="btn btn-primary btn-lg"> Search </button>
             </div>
 
 
@@ -50,7 +51,7 @@ function SearchBar() {
                 {
                     suggArray.map((obj) => {
                         return (
-                            <button className="flex w-full" > {obj["2. name"]} </button>
+                            <button className="btn btn-block" > {obj["2. name"]} </button>
                         )
                     })
                 }
