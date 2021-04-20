1c8c9380f0a7637223ff32c9807a8d65c8b906d7
Bhavin <bhavin1625p@gmail.com>
2021-04-20 13:09:52 +0530

    css added

--- client/stock-tracker/src/Pages/Aboutus.js
+++ client/stock-tracker/src/Pages/Aboutus.js
@@ -6,19 +6,19 @@ function Aboutus() {
         <>
             <Navbar />
             <div className="jumbotron flex m-lg-5 p-lg-5">
-                <h1> <b>ABOUT US</b></h1>
-                <br />
-                There are a lot of stocks in the market to select a stock. 
-                We need to do a proper study of the previous trends and data. 
-                Also after going through all that trouble it's difficult to manage the stocks in our portfolio and keeping track of them all at once. 
-                Hence, we decided to address this issue and created this website to ease the process.
-                <br />
-                <br />
-                Created by:
-                <br />
-                Bhavin Patel
-                <br />
-                Parth Pawar
+                <div className="container w-50">
+                    <h1> <b>ABOUT US</b></h1>
+                    <br />
+                    <h5 className="font-weight-lighter">
+                        There are a lot of stocks in the market to select a stock. <br />
+                        We need to do a proper study of the previous trends and data. <br />
+                        Also after going through all that trouble it's difficult to manage the stocks in our portfolio and keeping track of them all at once. <br />
+                        Hence, we decided to address this issue and created this website to ease the process. <br />
+                    </h5>
+                    <h6 className="d-flex justify-content-end">- Stocks Tracker</h6>
+                    <br />
+                    <br />
+                </div>
             </div>
         </>
     )
